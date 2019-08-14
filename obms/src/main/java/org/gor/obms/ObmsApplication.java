20b89ab58221b28af6e9db0957555055d8cb0989
SaileshVz <sailesh.vz@outlook.com>
2019-08-14 12:46:26 +0530

    Correction added Loadbalanced annotation

--- obms/src/main/java/org/gor/obms/ObmsApplication.java
+++ obms/src/main/java/org/gor/obms/ObmsApplication.java
@@ -2,6 +2,7 @@ package org.gor.obms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -27,6 +28,7 @@ public class ObmsApplication {
     }
 	
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate(){
 		return new RestTemplate();
 	}
